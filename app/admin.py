badae028f2f394785f7cd150a1271d631356cf8e
danchello <danchello.man@gmail.com>
2023-01-28 14:36:33 +0600

    Настроил вывод моделей в файле admin.py вместе с GeoDjango, и создал файл requirement.txt в который записал зависимости которые установил

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,39 @@
 from django.contrib import admin
 
-# Register your models here.
+from app.models import Season, Culture, Farmer, Plots
+
+
+@admin.register(Farmer)
+class FarmerAdmin(admin.ModelAdmin):
+    model = Farmer
+
+
+@admin.register(Culture)
+class FarmerAdmin(admin.ModelAdmin):
+    model = Culture
+
+
+@admin.register(Season)
+class FarmerAdmin(admin.ModelAdmin):
+    model = Season
+
+
+@admin.register(Plots)
+class PlotAdmin(admin.ModelAdmin):
+    list_display = ('id', 'get_farmers', 'get_cultures', 'get_seasons')
+    list_display_links = ('id', 'get_farmers',)
+    list_filter = ('farmer__name', 'season__how_season', 'culture__name')
+    save_on_top = True
+    save_as = True
+
+    def get_farmers(self, obj):
+        return obj.farmer.name
+
+    def get_cultures(self, obj):
+        if obj.culture is not None:
+            return obj.culture.name
+        else:
+            return obj.culture
+
+    def get_seasons(self, obj):
+        return obj.season.how_season
